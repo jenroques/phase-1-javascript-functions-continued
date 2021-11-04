9f4efe3fd424f9dd42d9d7ab5244f3ee1fdf9775
Jennifer <jenniferroques@gmail.com>
2021-11-04 16:08:52 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ function saturdayFun(activity="roller-skate") {
   return `This Saturday, I want to ${activity}!`
 }
 
-let mondayWork = function(activity="go to the office") {
-  return `This Monday, I will ${activity}.`
-}
 
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
-    return `You are ${style}${adjective}${style}!`
-  }
+let mondayWork = function(activity="go to the office") {
+   return `This Monday, I will ${activity}.`
+} 
+
+let wrapAdjective = function(star ="*") {
+    return function(adj="special") {
+    return `You are ${star}${adj}${star}!`
+    }
+  return inner; 
 }
